9f24022eae72109fbf04433296608e90de46b9ec
Daniel Brooks <dbrooks@dabsquared.com>
2017-08-12 00:01:05 -0700

    Upgrading

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,12 +11,28 @@ if [ "$1" = 'supervisord' ]; then
     fi
 fi
 
-if [ "$WAIT_FOR_PHP" == "true" ]; then
-    while true
-    do
-      [ -f .php_setup ] && break
-      sleep 2
-    done
+
+if [[ -z "$GIT_REPO" && -z "$GIT_SSH_KEY" ]]
+then
+    echo "No GIT Repository defined, not pulling."
+    if [ "$WAIT_FOR_PHP" == "true" ]; then
+        while true
+        do
+          [ -f .php_setup ] && break
+          sleep 2
+        done
+    fi
+else
+    echo "Pulling GIT Repository to /var/www/symfony"
+    eval $(ssh-agent -s)
+    echo "$GIT_SSH_KEY" > test.key
+    chmod -R 600 test.key
+    ssh-add test.key
+    mkdir -p ~/.ssh
+    [[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+    cd /var/www
+    git clone "$GIT_REPO" symfony
+    /setup.sh
 fi
 
 exec "$@"
