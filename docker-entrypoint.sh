3e1811bbc060922d9eb94a246fdfc7e3c280724a
Daniel <bassrock@users.noreply.github.com>
2018-02-18 17:22:27 -0800

    Updating docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,37 +1,11 @@
 #!/bin/bash
 set -e
 
-if [ "$1" = 'supervisord' ]; then
-    if [[ -z "$GIT_REPO" && -z "$GIT_SSH_KEY" ]]
-    then
-        echo "No GIT Repository defined, not pulling."
-        if [ "$WAIT_FOR_PHP" == "true" ]; then
-            while true
-            do
-              [ -f .php_setup ] && break
-              sleep 2
-            done
-        fi
-    else
-        echo "Pulling GIT Repository to /var/www/symfony"
-        mkdir -p ~/.ssh
-        eval "$(ssh-agent)" && ssh-agent -s
-        echo "$GIT_SSH_KEY" > ~/.ssh/id_rsa
-        chmod -R 0600 ~/.ssh/id_rsa
-        ssh-add ~/.ssh/id_rsa
-        [[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
-        cd /var/www
-        git clone --depth 1 "$GIT_REPO" symfony
-        /setup.sh
-    fi
-
-	if [ "$SUPERVISOR_CONF" ]; then
-        cp /var/www/symfony/$SUPERVISOR_CONF /etc/supervisor/conf.d/supervisord.conf
-        sed -i "s|REDISPREFIX|$symfony_redis_prefix|g" /etc/supervisor/conf.d/supervisord.conf
-        sed -i "s|REDISHOST|$symfony_redis_host|g" /etc/supervisor/conf.d/supervisord.conf
-        sed -i "s|REDISPORT|$symfony_redis_port|g" /etc/supervisor/conf.d/supervisord.conf
-        sed -i "s|REDISDB|$symfony_redis_database|g" /etc/supervisor/conf.d/supervisord.conf
-    fi
+if [ "$1" = 'supervisord' ]; then   
+	if [ "$ISDEV" == "false" ]; then
+		php -d newrelic.appname="$symfony_app_name" bin/console --env="$ENVIRONMENT" doctrine:migrations:migrate --no-interaction || (echo >&2 "Doctrine Migrations Failed" && exit 1)
+		php -d newrelic.appname="$symfony_app_name" bin/console --env="$ENVIRONMENT" cache:warmup || (echo >&2 "Cache Warmup Dev Failed" && exit 1)
+	fi
 fi
 
 
