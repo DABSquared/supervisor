30afca65f09681b2839ebbc74f59b8ed045041c7
Daniel Brooks <bass_rock@me.com>
2018-09-03 22:16:14 -0700

    Updating supervisor

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,11 +8,11 @@ if [ "$1" = 'supervisord' ]; then
 	fi
 	
 	if [ "$SUPERVISOR_CONF" ]; then
-	 	cp /var/www/symfony/$SUPERVISOR_CONF /etc/supervisor/conf.d/supervisord.conf
-	 	sed -i "s|REDISPREFIX|$symfony_redis_prefix|g" /etc/supervisor/conf.d/supervisord.conf
-	 	sed -i "s|REDISHOST|$symfony_redis_host|g" /etc/supervisor/conf.d/supervisord.conf
-	 	sed -i "s|REDISPORT|$symfony_redis_port|g" /etc/supervisor/conf.d/supervisord.conf
-		sed -i "s|REDISDB|$symfony_redis_database|g" /etc/supervisor/conf.d/supervisord.conf
+	 	cp /var/www/symfony/$SUPERVISOR_CONF /etc/supervisord.conf
+	 	sed -i "s|REDISPREFIX|$symfony_redis_prefix|g" /etc/supervisord.conf
+	 	sed -i "s|REDISHOST|$symfony_redis_host|g" /etc/supervisord.conf
+	 	sed -i "s|REDISPORT|$symfony_redis_port|g" /etc/supervisord.conf
+		sed -i "s|REDISDB|$symfony_redis_database|g" /etc/supervisord.conf
  	fi
 fi
 
